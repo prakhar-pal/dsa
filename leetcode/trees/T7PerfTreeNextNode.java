2fd7b1a576c29d51680c99cf797551ad49f62d5b
Prakhar <prakharpalpfdr+github@gmail.com>
2021-08-08 22:32:51 +0530

    leetcode: implment updating next node for each node in perfect tree

---
+++ leetcode/trees/T7PerfTreeNextNode.java
@@ -0,0 +1,68 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+//https://leetcode.com/explore/learn/card/data-structure-tree/133/conclusion/994/
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) return null;
+        else {
+            if(root.left != null){
+                root.left.next = root.right;
+            }
+            if(root.right != null && root.next != null){
+                root.right.next = root.next.left;
+            }
+            connect(root.left);
+            connect(root.right);
+        }
+        return root;
+    }
+}
+
+public class T7NextRightNode {
+    public static void printSol(Node root){
+        Queue<Node> q = new LinkedList<Node>();
+        while(root != null){
+            Node temp = root;
+            while(temp != null){
+                System.out.print(temp.val + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+            root = root.left;
+        }
+    }
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        Solution sol = new Solution();
+        sol.connect(root);
+        printSol(root);
+    }
+}
