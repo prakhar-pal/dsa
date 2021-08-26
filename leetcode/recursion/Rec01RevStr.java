081168b3ec86e357d36978ca14b0130462bcb1a5
Prakhar <prakharpalpfdr+github@gmail.com>
2021-08-27 00:48:55 +0530

    [LC][Rrecusion]: reverse a string

---
+++ leetcode/recursion/Rec01RevStr.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1440/
+
+class Solution {
+    public void reverseString(char[] s) {
+        int i=0, j=s.length-1;
+        reverseString(s,i,j);
+    }
+    private void reverseString(char[] s, int i, int j){
+        if(i>j){
+            return;
+        }else {
+            reverseString(s, i+1, j-1);
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
+}
+
+public class Rec01RevStr {
+    public static void main(String[] args) {
+        char[] s1 = {'h','e','l','l','o'};
+        char[] s2 = {'H','a','n','n','a','h'};
+        Solution sol = new Solution();
+        sol.reverseString(s1);
+        sol.reverseString(s2);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
