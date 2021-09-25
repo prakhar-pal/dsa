6e8ec7af0a366ea5ea077fc894c6f8d1d7c66e3b
Prakhar <prakharpalpfdr+github@gmail.com>
2021-09-25 12:47:14 +0530

    Parag's question on subsets of strings

---
+++ leetcode/P1StringSubsetsRecursive.java
@@ -0,0 +1,45 @@
+// print all subsets of a given string
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Solution {
+    public List<String> getAllSubstrings(String str){
+        List<String> list = new ArrayList<>();
+        if(str == null){
+            list.add("");
+            return list;
+        }
+        else {
+            list.add("");
+            list.add(str.charAt(0)+"");
+            if(str.length() == 1) return list;
+            else {
+                List<String> sublist = getAllSubstrings(str.substring(1));
+                List<String> finalList = new ArrayList<>();
+                for(String ch: list){
+                    for(String finalListStr: sublist){
+                        finalList.add(ch.concat(finalListStr));
+                    }
+                }
+                return finalList;
+            }
+        }
+    }
+}
+
+public class P1StringSubsetsRecursive {
+    public static void main(String[] args){
+        // String str = "abc";
+        String str;
+        Scanner sc = new Scanner(System.in);
+        str = sc.nextLine();
+        Solution sol = new Solution();
+        List<String> list = sol.getAllSubstrings(str);
+        for(String s: list){
+            if(s.equals("")) System.out.print("-\t");
+            else System.out.print(s+"\t");
+        }
+        System.out.println();
+    }
+}
