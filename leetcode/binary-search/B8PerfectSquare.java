fa56c5f648e9d182bedd36552f8f2f7287c0e927
Prakhar <prakharpalpfdr+github@gmail.com>
2021-11-07 18:41:22 +0530

    [LC][binary-search] perfect square and median of sorted array solution

---
+++ leetcode/binary-search/B8PerfectSquare.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/explore/learn/card/binary-search/137/conclusion/978/
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        int MAX = (int)Math.sqrt(Math.pow(2,31)-1);
+        int low = 1, high = Math.min(Math.max(1,num/2), MAX);
+        boolean ps = false;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            int sq = mid*mid;
+            if(sq == num){
+                ps = true;
+                break;
+            }
+            else if(sq > num) {
+                high = mid - 1;
+            }else {
+                low = mid + 1;
+            }
+        }
+        System.out.println("low: "+low+" high: "+high);
+        return ps;
+    }
+}
+
+class B8PerfectSquare {
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        assert sol.isPerfectSquare(4) == true;
+        assert sol.isPerfectSquare(1) == true;
+        assert sol.isPerfectSquare(2) == false;
+        assert sol.isPerfectSquare(4) == true;
+        assert sol.isPerfectSquare((int)Math.pow(2,30)+1) == false;
+        assert sol.isPerfectSquare(808201) == true;
+    }
+}
