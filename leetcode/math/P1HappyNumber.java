cbeb689c980b3ff363e225d28e7a75f892c0cfac
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-03-19 00:45:18 +0530

    [LC][math]: added another solution for detecting if a given number is a happy number

--- leetcode/math/P1HappyNumber.java
+++ leetcode/math/P1HappyNumber.java
@@ -37,9 +37,28 @@ class Solution {
 }
 
 
+class Solution2 {
+    public int goHappyNext(int n) {
+        int squaredSum = 0;
+        while(n > 0) {
+            int remainder = n % 10;
+            n = n/10;
+            squaredSum += remainder * remainder;
+        }
+        return squaredSum;
+    }
+    public boolean isHappy(int n) {
+        while(n >= 10) {
+            n = goHappyNext(n);
+        }
+        return n == 1;
+    }
+}
+
+
 class P1HappyNumber {
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        Solution2 sol = new Solution2();
         assert sol.isHappy(19);
         assert !sol.isHappy(2);
         assert sol.isHappy(13);
