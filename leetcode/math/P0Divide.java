2daacb1e0118b7e0dbcac7b4be8899041c145931
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-03-18 23:27:36 +0530

    chore: added solution for divide integers

---
+++ leetcode/math/P0Divide.java
@@ -0,0 +1,37 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-medium/113/math/820/
+class Solution {
+    public static long LOWEST_QUOTIENT = (long)Math.pow(2, 31) * (-1);
+    public static long HIGHEST_QUOTIENT = (long) (Math.pow(2, 31) - 1);
+    public long abs(long num) {
+        if(num >=0) {
+            return num;
+        }else {
+            return num * -1;
+        }
+    }
+    public int divide(long dividend, long divisor) {
+        boolean isPositive = true;
+        if((dividend > 0 && divisor < 0 )|| (divisor > 0 && dividend < 0 )) {
+            isPositive = false;
+        }
+        dividend = this.abs(dividend);
+        divisor = this.abs(divisor);
+        long result = Math.floorDiv(dividend, divisor) * (isPositive ? 1l: -1l);
+        if(result < LOWEST_QUOTIENT) {
+            return (int)LOWEST_QUOTIENT;
+        }else if(result > HIGHEST_QUOTIENT) {
+            return (int)HIGHEST_QUOTIENT;
+        }
+
+        return (int)result;
+    }
+}
+
+class P0Divide {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        assert sol.divide(10, 3) == 3;
+        assert sol.divide(7, -3) == -2;
+        assert sol.divide(-2147483648, -1) == 2147483647;
+    }
+}
