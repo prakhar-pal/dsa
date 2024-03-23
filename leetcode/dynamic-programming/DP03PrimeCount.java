e3dedc13d64cd0a1582691b8a626e4809d045959
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-03-23 23:32:52 +0530

    [LC][sieve]chore: added alternate solution for faster sieve at the cost of memory usage

--- leetcode/dynamic-programming/DP03PrimeCount.java
+++ leetcode/dynamic-programming/DP03PrimeCount.java
@@ -1,4 +1,8 @@
 class Sieve {
+    /**
+     * run time, beats solutions, memory, beats memory usage
+     * 50ms,  98.17%, 45.20MB, 99.23% with java
+     */
     private int MAX_N = (int)Math.pow(10, 6) * 5;
     /** AKA sieve of Eratosthens */
     private boolean[] isPrime = new boolean[MAX_N+1];
@@ -28,6 +32,44 @@ class Sieve {
             total +=  isPrime[i] ? 1 : 0;
         }
         return total;
+    }
+}
+
+class Sieve2 {
+    /**
+     * run time, beats solutions, memory, beats memory usage
+     * 32ms, 99.13%, 66.34MB, 5.6% with Java
+     */
+    private int MAX_N = (int)Math.pow(10, 6) * 5;
+    /** AKA sieve of Eratosthens */
+    private boolean[] isPrime = new boolean[MAX_N+1];
+    private int[] primeCount = new int[MAX_N+1];
+    public Sieve2() {
+        /** assume all ints are prime */
+        for(int i=0;i<=MAX_N;i++) {
+            isPrime[i] = true;
+        }
+        isPrime[0] = false;
+        isPrime[1] = false;
+
+        for(int i=2;i*i<=MAX_N; i+=1) {
+            if(isPrime[i]) {
+                int currentNum = i*i;
+                while(currentNum <= MAX_N) {
+                    this.isPrime[currentNum] = false;
+                    currentNum += i;
+                }
+            }
+        }
+        /*pre-compute all the possible counts of premium numbers at index i*/
+        for(int i=2; i<=MAX_N;i++) {
+            primeCount[i] = primeCount[i-1] + (isPrime[i] ? 1 : 0);
+        }
+    }
+
+    public int countPrimes(int upperLimit) {
+        int n = Math.max(1, upperLimit - 1);
+        return primeCount[n];
     }
 }
 class Solution {
