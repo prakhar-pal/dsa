f0f6f89cf563b36b41a80b5c0bbe5f551f5338d6
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-03-27 22:55:21 +0530

    [LC]: solution for converting excel sheet column name to column number

---
+++ leetcode/math/P3ExcelColumnNumber.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/excel-sheet-column-number/
+
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        char a = 'A';
+        int column = 0;
+        int endIndex = columnTitle.length()-1;
+        for(int i=endIndex;i>=0;i--) {
+            int power = endIndex - i;
+            column+= (columnTitle.charAt(i) - a + 1)*Math.pow(26, power);
+        }
+        return column;
+    }
+}
+
+class P3ExcelColumnNumber {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        assert sol.titleToNumber("A") == 1;
+        assert sol.titleToNumber("AB") == 28;
+        assert sol.titleToNumber("ZY") == 701;
+    }
+}
