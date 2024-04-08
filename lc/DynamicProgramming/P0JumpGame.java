7a3073769099e78f0ee2ae8fcceffe87aaca1d4b
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-04-08 14:23:08 +0530

    [LC][DP]: added greedy solution for jump game problem

---
+++ lc/DynamicProgramming/P0JumpGame.java
@@ -0,0 +1,44 @@
+package lc.DynamicProgramming;
+/**
+ * https://leetcode.com/problems/jump-game/description/
+ */
+
+public class P0JumpGame {
+    public static void main(String[] args) {
+        P0Solution sol = new P0SolutionOne();
+
+        assert sol.canJump(new int[] {2,3,1,1,4}) == true;
+        assert sol.canJump(new int[] {3,2,1,0,4}) == false;
+        assert sol.canJump(new int[] {0});
+    }
+}
+
+interface P0Solution {
+    public boolean canJump(int[] nums);
+}
+
+class P0SolutionOne implements P0Solution{
+    /**
+     * Runtime 2ms
+     * Beats 82.18% of users with Java
+     * Memory 45.59MB, Beats 53.41% of users with Java
+     */
+    public boolean canJump(int[] nums) {
+        return canReach(nums, nums.length-1);
+    }
+    private boolean canReach(int[] nums, int index) {
+        if(index < 0) {
+            return false;
+        }
+        for(int i=index-1;i>=0;i--) {
+            boolean canJumpToIndex = index <= (i + nums[i]);
+            if(canJumpToIndex) {
+                if(i == 0) {
+                    return true;
+                }
+                return canReach(nums, i);
+            }
+        }
+        return index == 0;
+    }
+}
