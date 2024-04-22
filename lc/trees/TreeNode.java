6937807a08873d71ac420cedafffe205db00c156
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-04-22 16:21:38 +0530

    [LC] added solution for making height balanced BSTs from sorted array

--- lc/trees/TreeNode.java
+++ lc/trees/TreeNode.java
@@ -1,4 +1,6 @@
 package lc.trees;
+import java.util.*;
+
 /**
  Definition for a binary tree node.
  */
@@ -12,5 +14,25 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public void printLevelOrder() {
+        List<TreeNode> list = new ArrayList<>();
+        list.add(this);
+        int index = 0;
+        while(list.size() != 0) {
+            List<TreeNode> newList = new ArrayList<>();
+            System.out.print("row="+index+"\t");
+            for(TreeNode node: list) {
+                if(node != null) {
+                    newList.add(node.left);
+                    newList.add(node.right);
+                }
+                System.out.print((node != null ? node.val : null) + "\t");
+            }
+            System.out.println();
+            index++;
+            list = newList;
+        }
     }
 }
