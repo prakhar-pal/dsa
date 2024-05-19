cc6eda93a800ae5187bbe77e484be940e3056363
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-05-20 03:43:55 +0530

    [LC]: added solution for wiggle sort
    also refactored names for rest of the solutions in sorting folder

--- lc/Sorting/S05MergeSortedArrays.java
+++ lc/Sorting/S05MergeSortedArrays.java
@@ -8,7 +8,7 @@ import lc.ArraysAndStrings.ArrayUtils;
  * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
  * The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
  */
-public class SO01MergeSortedArrays {
+public class S05MergeSortedArrays {
     public static void main(String[] args) {
         SO01Solution sol = new SO01Solution();
 
