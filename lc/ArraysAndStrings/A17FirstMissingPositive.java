a6488ef5269a5b6953e1784c47f0e574482a9ad2
Prakhar Pal <prakharpalpfdr+github@gmail.com>
2024-06-25 03:14:45 +0530

    [LC]: added solution for finding first missing positive number

---
+++ lc/ArraysAndStrings/A17FirstMissingPositive.java
@@ -0,0 +1,46 @@
+package lc.ArraysAndStrings;
+/**
+ * https://leetcode.com/problems/first-missing-positive/submissions/
+ */
+public class A17FirstMissingPositive {
+    public static void main(String[] args) {
+        A17Solution solution = new A17Solution();
+        assert solution.firstMissingPositive(new int[] {1,2,0}) == 3;
+        assert solution.firstMissingPositive(new int[] {3,4,-1,1}) == 2;
+        assert solution.firstMissingPositive(new int[] {7,8,9,11,12}) == 1;
+        assert solution.firstMissingPositive(new int[] {1,2,3, (int)Math.pow(2, 31)}) == 4;
+    }
+}
+
+class A17Solution {
+    public int firstMissingPositive(int[] nums) {
+        int positiveCount = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int n:nums) {
+            if(n > 0) {
+                min = Math.min(n, min);
+                max = Math.max(n, max);
+                positiveCount++;
+            }
+        }
+        int index = 0;
+        int[] positives = new int[positiveCount];
+        for(int n:nums) {
+            if(n>0) {
+                positives[index++] = n;
+            }
+        }
+        int size = (int) Math.pow(10, 5);
+        boolean[] isPresent = new boolean[size];
+        for(int n: positives) {
+            if(n <= size) {
+                isPresent[n-1] = true;
+            }
+        }
+        for(int i=0;i<size;i++) {
+            if(!isPresent[i]) {
+                return i+1;
+            }
+        }
+        return (int) Math.pow(10, 5) + 1;
+    }
+}
