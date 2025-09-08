a510aeac8521adb9d441798bfd8a0a2c60413df7
Prakhar Pal <prakharpalpfdr+github.com>
2025-09-09 05:06:50 +0530

    added another solution for maximal square (dynamic programming)

--- lc/DynamicProgramming/DP19MaximalSquare.java
+++ lc/DynamicProgramming/DP19MaximalSquare.java
@@ -2,7 +2,7 @@ package lc.DynamicProgramming;
 // https://leetcode.com/problems/maximal-square/description/?envType=problem-list-v2&envId=matrix
 class DP19MaximalSquare {
      public static void main(String[] args) {
-          Dp19Solution sol = new Dp19Solution();
+          Dp19Solution2 sol = new Dp19Solution2();
           assert sol.maximalSquare(new char[][] {{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}}) == 4;
           assert sol.maximalSquare(new char[][] {{'0','1'},{'1','0'}}) == 1;
           assert sol.maximalSquare(new char[][] {{'1','1','1','0','0'},{'1','1','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}}) == 9;
@@ -27,7 +27,6 @@ class Dp19Solution {
                     maxi = Math.max(maxi, dp(matrix, i, j));
                }
           }
-          System.out.println("maxi="+maxi);
           return maxi * maxi;
      }
 
@@ -45,6 +44,42 @@ class Dp19Solution {
                     pointer++;
                }
                counter[i][j] = 1 + Math.min(borderSize, innerDp);
+          }
+          return counter[i][j];
+     }
+}
+
+class Dp19Solution2 {
+     int[][] counter;
+     int maxi;
+
+     public int maximalSquare(char[][] matrix) {
+          int n = matrix.length, m = matrix[0].length;
+          counter = new int[n][m];
+          maxi = Integer.MIN_VALUE;
+          for(int i=0;i<n;i++) {
+               for(int j=0;j<m;j++) {             
+                   counter[i][j] = -1;
+               }
+          }
+          for(int i=0;i<n;i++) {
+               for(int j=0;j<m;j++) {             
+                    maxi = Math.max(maxi, dp(matrix, i, j));
+               }
+          }
+          return maxi * maxi;
+     }
+
+     private int dp(char[][] matrix, int i, int j) {
+          int n = matrix.length, m = matrix[0].length;
+          if(i >= n || j >= m || matrix[i][j] == '0') {
+               return 0;
+          }
+          if(counter[i][j] < 0) {
+               int diag = dp(matrix, i + 1, j + 1);
+               int right = dp(matrix, i, j + 1);
+               int bottom = dp(matrix, i + 1, j);
+               counter[i][j] = 1 + Math.min(Math.min(right, bottom), diag);
           }
           return counter[i][j];
      }
