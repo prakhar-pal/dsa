c811ae69b5400f9e5758655da0063b9874fdaea8
Prakhar Pal <prakharpalpfdr+github.com>
2025-09-18 06:45:04 +0530

    (leetcode) solution for k-pairs-minimum-sum

--- lc/utils/ArrayUtils.java
+++ lc/utils/ArrayUtils.java
@@ -1,6 +1,8 @@
 package lc.utils;
 import java.lang.reflect.Array;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ArrayUtils {
     public static <T> boolean isSame1DArray(T[] arr1, T[] arr2) {
@@ -50,6 +52,23 @@ public class ArrayUtils {
         }
         return true;
     }
+    public static boolean isSame2DArray(Integer[][] arr1, Integer[][] arr2) {
+        if(arr1.length != arr2.length) {
+            return false;
+        }
+        if(arr1[0].length != arr2[0].length) {
+            return false;
+        }
+        int m = arr1.length, n = arr1[0].length;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(arr1[i][j] != arr2[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public static Integer[] convert(int[] arr) {
         Integer[] copy = new Integer[arr.length];
@@ -67,6 +86,18 @@ public class ArrayUtils {
         T[] array = (T[]) list.toArray();
         return array;
     }
+
+    // public static <T> T[][] listTo2DArray(List<List<T>> list) {
+    //     T[][] arr = new T[list.size()][];
+    //     for (int i = 0; i < list.size(); i++) {
+    //         List<T> row = list.get(i);
+    //         arr[i] = new T[row.size()];
+    //         for (int j = 0; j < row.size(); j++) {
+    //             arr[i][j] = row.get(j);
+    //         }
+    //     }
+    //     return arr;
+    // }
 
    public static <T> T[][] to2DArray(List<List<T>> list, Class<T> clazz) {
         int rows = list.size();
